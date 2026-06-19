fe064de8ef09f9d074fbb818c4b3dce0b0df01ae
marmitravel26 <marmitravel26@gmail.com>
2026-06-19 09:02:18 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,6 @@
+name = input("Как тебя зовут?")
+print("Привет,",name)
+money = int(input("Сколько у тебя накоплений? ")) 
+salary = int(input("Какая зарплата в месяц?"))
+total = money + salary * 10
+print("Через 10 месяцев будет:",total)
